67b2184f122fdee109df021b81df308d1ada7ca2
François Pinard <pinard@iro.umontreal.ca>
2013-02-19 21:15:27 -0500

    poporg.el: Tiny editorial changes.

--- poporg.el
+++ poporg.el
@@ -2,20 +2,23 @@
 
 ;; Copyright © 2013 Ubity inc.
 
-;; Author: François Pinard
+;; Author: François Pinard <pinard@iro.umontreal.ca>
+;; Maintainer: François Pinard <pinard@iro.umontreal.ca>
+;; URL: https://github.com/pinard/poporg
 
-;; This is free software: you can redistribute it and/or modify it
+;; This is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-;; License for more details.
-
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ;;; Commentary:
 
