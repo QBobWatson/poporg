fc14740d120045245d7ba7b5084f5c2c3c9659c4
Joe Rabinoff <jrabinoff@math.gatech.edu>
2014-06-16 09:27:14 -0400

    Added a brief description to the Commentary.

--- poporg.el
+++ poporg.el
@@ -25,8 +25,13 @@
 
 ;;; Commentary:
 
+;; poporg is a small Emacs Lisp project to help editing program strings and
+;; comments using Org mode (or any other major mode).  This can be useful as it
+;; is often more convenient to edit large pieces of text, like Emacs Lisp or
+;; Python docstrings, in an org-mode buffer instead of in a comment or a string.
+
 ;; See the README.org file located at https://github.com/QBobWatson/poporg for
-;; package usage information.
+;; detailed usage information.
 
 ;;; Code:
 
